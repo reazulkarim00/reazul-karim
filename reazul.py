e2437fbae4486b1119a32d33a87e18ad8af4de3a
reazulkarim00 <125479665+reazulkarim00@users.noreply.github.com>
2023-02-15 21:11:48 +0600

    git clone https://github.com/reazul-karim

--- reazul.py
+++ reazul.py
@@ -1,10 +1,10 @@
-#apt update
-#apt upgrade
-#apt install python
-#apt install python2
-#pip2 install requests 
-#pip2 install mechanize 
+apt update
+apt upgrade
+apt install python
+apt install python2
+pip2 install requests 
+pip2 install mechanize 
 apt install git
 git clone https://github.com/reazul-karim
-# reazul-karim
+reazul-karim
 reazulkarim00
